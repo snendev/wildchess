8ed5cc2b60e729b76d562f7494bc93e6580d4963
Sean Sullivan <sullivansean27@gmail.com>
2023-08-23 04:33:09 -0400

    improve structure of system in ui

--- ui/src/ui.rs
+++ ui/src/ui.rs
@@ -1,4 +1,5 @@
 use bevy::{
+    ecs::query::WorldQuery,
     prelude::{Entity, EventWriter, Local, Query, ResMut, With},
     utils::HashMap,
 };
@@ -50,28 +51,46 @@ fn describe_pattern(pattern: &Pattern) -> egui::RichText {
     .size(24.)
 }
 
-pub type PieceQuery<'a> = (
-    Entity,
-    &'a Behavior,
-    &'a Position,
-    &'a StartPosition,
-    &'a Team,
-    &'a Targets,
-    &'a PieceKind,
-    Option<&'a PieceIcon>,
-    Option<&'a Promotable>,
-);
-pub type PieceTuple<'a> = (
-    Entity,
-    Behavior,
-    Position,
-    StartPosition,
-    Team,
-    Targets,
-    PieceKind,
-    Option<&'a PieceIcon>,
-    Option<&'a Promotable>,
-);
+#[derive(WorldQuery)]
+pub struct PieceQuery {
+    pub entity: Entity,
+    pub behavior: &'static Behavior,
+    pub position: &'static Position,
+    pub start_position: &'static StartPosition,
+    pub team: &'static Team,
+    pub targets: &'static Targets,
+    pub kind: &'static PieceKind,
+    pub promotable: Option<&'static Promotable>,
+    pub icon: Option<&'static PieceIcon>,
+}
+
+pub struct PieceData<'a> {
+    pub entity: Entity,
+    pub behavior: &'a Behavior,
+    pub position: &'a Position,
+    pub start_position: &'a StartPosition,
+    pub team: &'a Team,
+    pub targets: &'a Targets,
+    pub kind: &'a PieceKind,
+    pub promotable: Option<&'a Promotable>,
+    pub icon: Option<&'a PieceIcon>,
+}
+
+impl<'a> From<PieceQueryItem<'a>> for PieceData<'a> {
+    fn from(piece: PieceQueryItem<'a>) -> Self {
+        PieceData {
+            entity: piece.entity,
+            behavior: piece.behavior,
+            position: piece.position,
+            start_position: piece.start_position,
+            team: piece.team,
+            targets: piece.targets,
+            kind: piece.kind,
+            promotable: piece.promotable,
+            icon: piece.icon,
+        }
+    }
+}
 
 #[allow(clippy::too_many_arguments)]
 pub fn egui_chessboard(
@@ -85,46 +104,14 @@ pub fn egui_chessboard(
 ) {
     let Ok(team_with_turn) = player_query.get_single() else { return };
 
-    let pieces: HashMap<Square, PieceTuple> = piece_query
-        .iter()
-        .map(
-            |(
-                entity,
-                behavior,
-                position,
-                start_position,
-                team,
-                vision,
-                piece,
-                icon,
-                promotable,
-            )| {
-                (
-                    position.0,
-                    (
-                        entity,
-                        behavior.clone(),
-                        position.clone(),
-                        start_position.clone(),
-                        *team,
-                        vision.clone(),
-                        *piece,
-                        icon,
-                        promotable,
-                    ),
-                )
-            },
-        )
+    let pieces: HashMap<Square, PieceData> = piece_query
+        .into_iter()
+        .map(|query| (query.position.0, query.into()))
         .collect();
 
     let selected_piece_entity = selected_piece.as_ref();
     let selected_piece_data = selected_piece_entity
-        .and_then(|entity| {
-            piece_query
-                .get(*entity)
-                .map(|(_, _, position, _, _, _, _, _, _)| position.0)
-                .ok()
-        })
+        .and_then(|entity| piece_query.get(*entity).map(|piece| piece.position.0).ok())
         .and_then(|square| pieces.get(&square));
 
     let ctx = contexts.ctx_mut();
@@ -135,21 +122,16 @@ pub fn egui_chessboard(
                     for x in 0..=7 {
                         let square = Square::new(y.try_into().unwrap(), x.try_into().unwrap());
 
-                        let piece = pieces.get(&square).map(
-                            |(_, _, _, start_position, team, _, _, icon, _)| {
-                                (start_position, team, icon)
-                            },
-                        );
+                        let piece = pieces
+                            .get(&square)
+                            .map(|piece| (piece.start_position, piece.team, piece.icon));
 
                         let background_color =
                             get_square_background(x, y, selected_piece_data, &square);
                         let stroke_color = get_square_stroke(selected_piece_data, &square);
 
-                        let mut button = get_button_ui(
-                            &*ctx,
-                            piece.and_then(|piece| *piece.2),
-                            background_color,
-                        );
+                        let mut button =
+                            get_button_ui(&*ctx, piece.and_then(|piece| piece.2), background_color);
                         if let Some(stroke_color) = stroke_color {
                             button = button.stroke(Stroke::new(4., stroke_color));
                         }
@@ -184,8 +166,8 @@ pub fn egui_chessboard(
                     promotion_writer.send(IssuePromotionEvent(movement, behavior));
                 }
 
-                if let Some((_, behavior, square, _, _, _, _, _, _)) = selected_piece_data {
-                    render_pattern_description(ui, behavior.clone(), square.0);
+                if let Some(piece) = selected_piece_data {
+                    render_pattern_description(ui, piece.behavior.clone(), piece.position.0);
                 }
             });
         });
@@ -198,12 +180,15 @@ const LIGHT_SQUARE_BG: Color32 = Color32::from_rgb(240, 217, 181);
 fn get_square_background(
     x: u16,
     y: u16,
-    selected_piece_data: Option<&PieceTuple>,
+    selected_piece_data: Option<&PieceData>,
     square: &Square,
 ) -> Color32 {
     let (is_target_square, can_attack_square) = selected_piece_data
-        .map(|(_, _, _, _, _, vision, _, _, _)| {
-            (vision.can_target(square), vision.can_attack(square))
+        .map(|piece| {
+            (
+                piece.targets.can_target(square),
+                piece.targets.can_attack(square),
+            )
         })
         .unwrap_or_else(|| (false, false));
     if is_target_square && can_attack_square {
@@ -217,9 +202,9 @@ fn get_square_background(
     }
 }
 
-fn get_square_stroke(selected_piece_data: Option<&PieceTuple>, square: &Square) -> Option<Color32> {
-    selected_piece_data.and_then(|(_, _, position, _, _, _, _, _, _)| {
-        if position.0 == *square {
+fn get_square_stroke(selected_piece_data: Option<&PieceData>, square: &Square) -> Option<Color32> {
+    selected_piece_data.and_then(|piece| {
+        if piece.position.0 == *square {
             Some(Color32::from_rgb(140, 140, 20))
         } else {
             None
@@ -249,25 +234,25 @@ fn get_button_ui(
 fn handle_clicked_square(
     square: Square,
     selected_piece: &mut Option<Entity>,
-    selected_piece_data: Option<&PieceTuple>,
-    pieces: &HashMap<Square, PieceTuple>,
+    selected_piece_data: Option<&PieceData>,
+    pieces: &HashMap<Square, PieceData>,
     move_writer: &mut EventWriter<IssueMoveEvent>,
     team_with_turn: &Team,
 ) {
-    if let Some((entity, _, _, _, team, vision, _, _, _)) = selected_piece_data {
-        if vision.can_target(&square) && team == team_with_turn {
+    if let Some(piece) = selected_piece_data {
+        if piece.targets.can_target(&square) && piece.team == team_with_turn {
             move_writer.send(IssueMoveEvent(Movement {
-                entity: *entity,
+                entity: piece.entity,
                 target_square: square,
             }));
             *selected_piece = None;
-        } else if let Some((current_entity, _, _, _, _, _, _, _, _)) = pieces.get(&square) {
-            *selected_piece = Some(*current_entity);
+        } else if let Some(piece) = pieces.get(&square) {
+            *selected_piece = Some(piece.entity);
         } else {
             *selected_piece = None;
         }
-    } else if let Some((current_entity, _, _, _, _, _, _, _, _)) = pieces.get(&square) {
-        *selected_piece = Some(*current_entity);
+    } else if let Some(piece) = pieces.get(&square) {
+        *selected_piece = Some(piece.entity);
     }
 }
 
