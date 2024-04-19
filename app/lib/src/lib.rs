715a9dd7f131e715fd7194143110716792f51abf
Sean Sullivan <sullivansean27@gmail.com>
2024-04-18 21:29:27 -0400

    incr res

--- app/lib/src/lib.rs
+++ app/lib/src/lib.rs
@@ -16,7 +16,7 @@ pub fn run_app(canvas: Option<String>) {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 canvas,
-                resolution: (1366., 768.).into(),
+                resolution: (1600., 900.).into(),
                 ..Default::default()
             }),
             ..Default::default()
