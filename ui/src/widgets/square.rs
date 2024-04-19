d86e73cf87daba2c8c50c02d4a750bdd41bb074e
Sean Sullivan <sullivansean27@gmail.com>
2024-04-18 21:29:44 -0400

    incr sqr size

--- ui/src/widgets/square.rs
+++ ui/src/widgets/square.rs
@@ -42,7 +42,7 @@ pub struct SquareWidget<'a> {
 }
 
 impl<'a> SquareWidget<'a> {
-    pub const WIDTH: f32 = 90.;
+    pub const WIDTH: f32 = 120.;
     const STROKE_WIDTH: f32 = 4.;
     const DARK_BG: Color32 = Color32::from_rgb(181, 136, 99);
     const LIGHT_BG: Color32 = Color32::from_rgb(240, 217, 181);
@@ -99,7 +99,7 @@ impl<'a> Widget for SquareWidget<'a> {
             }
             PieceIcon::Character(character) => {
                 let text = RichText::new(*character)
-                    .size(64.)
+                    .size(86.)
                     .strong()
                     .color(Color32::BLACK);
                 Button::new(text).fill(background_color)
